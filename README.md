9f318993a688ec72faf9cad465ccecd5b19b73cb
Will.Chen <will.chen@advantech.com.tw>
2018-12-24 14:12:37 +0800

    Add description about how to set the APN and socket type. 1. Modify the README.md for setting APN and socket type. 2. Remove some unused configurations for the mbed_app.json. 3. Alignment code style for the main.cpp.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # WISE-1570-PSM
 
+
 ## Introduction
+
 This example is demonstrated for power saving on WISE-1570.
+
 
 ## Deployment
 
@@ -17,9 +20,31 @@ Please follow the below steps to setup for development:
 
 1. `cd mbed-os; git apply ../patch/*`
 
+
+## Set the APN
+
+Set marco "MBED_CONF_APP_APN" to the APN provided by telecom in the main.cpp.
+
+```cpp
+
+    # define MBED_CONF_APP_APN          "your_apn_string"
+
+```
+
+## Select TCP or UDP
+
+Modify the file `mbed_app.json` to choose which socket type the application should use.
+
+```json
+
+     "sock-type": "TCP",
+
+```
+
 ## Compilation
 
-Goes into WISE-1570-PSM directory and runs the below script to compile the example:
+Goes into WISE-1570-PSM directory and runs the below script to compile the example.
 
 `./compile.sh`
+
 
--- main.cpp
+++ main.cpp
@@ -39,28 +39,28 @@
 //  
 // Power setting
 //
-#define PSM_PERIODIC_TIME			180	// 180 seconds
-#define PSM_ACTIVE_TIME				0 	// 0  second
+#define PSM_PERIODIC_TIME            180    // 180 seconds
+#define PSM_ACTIVE_TIME                0    // 0  second
 
 //
 // MCU sleep time interval
 //
-#define MCU_SLEEP_TIME	(PSM_PERIODIC_TIME * 1000) // ms
+#define MCU_SLEEP_TIME    (PSM_PERIODIC_TIME * 1000) // ms
 
 //
 // SIM pin code goes here
 //
 #ifndef MBED_CONF_APP_SIM_PIN_CODE
-# define MBED_CONF_APP_SIM_PIN_CODE	"1234"
+# define MBED_CONF_APP_SIM_PIN_CODE    "1234"
 #endif
 #ifndef MBED_CONF_APP_APN
-# define MBED_CONF_APP_APN			"internet.iot"
+# define MBED_CONF_APP_APN          "internet.iot.test.main"
 #endif
 #ifndef MBED_CONF_APP_USERNAME
-# define MBED_CONF_APP_USERNAME    	NULL
+# define MBED_CONF_APP_USERNAME     NULL
 #endif
 #ifndef MBED_CONF_APP_PASSWORD
-# define MBED_CONF_APP_PASSWORD   	NULL
+# define MBED_CONF_APP_PASSWORD     NULL
 #endif
 
 //
@@ -74,7 +74,7 @@
 // About echo server 
 //
 #define SEND_DATA_TIMEOUT 15000 // ms
-//#define YOUR_ECHO_SERVER_IP	1
+//#define YOUR_ECHO_SERVER_IP    1
 #if defined(YOUR_ECHO_SERVER_IP)
 #define HOST_NAME "your_host_name";
 #define PORT_NUM 9157
@@ -88,7 +88,7 @@
 //
 #define MAIN_DEBUG 1
 #define PRINT_TEXT_LENGTH 512
-#define EVENT_BUF_SIZE	128
+#define EVENT_BUF_SIZE    128
 #ifdef MAIN_DEBUG
 #define DMSG(s) do { print_function(s);} while(0)
 #else
@@ -161,7 +161,7 @@ void print_function(const char *input_string)
 
 void dot_event()
 {
-	while (true) {
+    while (true) {
         wait(4);
         if (!iface.is_connected()) {
             DMSG(".");
@@ -177,13 +177,13 @@ void dot_event()
 nsapi_error_t do_connect()
 {
     nsapi_error_t retcode;
-	uint8_t retry_counter = 0;
+    uint8_t retry_counter = 0;
 
     while (!iface.is_connected()) 
-	{
+    {
         retcode = iface.connect();
         if (retcode == NSAPI_ERROR_AUTH_FAILURE) {
-        	DMSG("\n\nAuthentication Failure. Exiting application\n");
+            DMSG("\n\nAuthentication Failure. Exiting application\n");
             return retcode;
         } else if (retcode != NSAPI_ERROR_OK) {
             snprintf(print_text, PRINT_TEXT_LENGTH, "\n\nCouldn't connect: %d, will retry\n", retcode);
@@ -200,7 +200,7 @@ nsapi_error_t do_connect()
     }
 
     DMSG("\n\nConnection Established.\n");
-	
+    
     return NSAPI_ERROR_OK;
 }
 
@@ -212,32 +212,32 @@ nsapi_error_t do_connect()
 nsapi_error_t test_send_recv()
 {
     int i;
-	int n = 0;
-	nsapi_size_or_error_t retcode;
+    int n = 0;
+    nsapi_size_or_error_t retcode;
     const char *echo_string = "Hello World";
     unsigned int send_data_size = strlen(echo_string);
-	
+    
 #if MBED_CONF_APP_SOCK_TYPE == TCP
     TCPSocket sock;
 #else
     UDPSocket sock;
 #endif
 
-	i = 0;
-	UARTSerial *modemUART = iface.get_serial();
-	SocketAddress sock_addr;
+    i = 0;
+    UARTSerial *modemUART = iface.get_serial();
+    SocketAddress sock_addr;
     while (i < NUM_PACKETS) 
-	{ 
-		//
-		// Turn on the power for peripherals
-		//
-		g_tPower = 1;
+    { 
+        //
+        // Turn on the power for peripherals
+        //
+        g_tPower = 1;
 
-		//
-		// Open socket
-		//
-    	retcode = sock.open(&iface);
-    	if (retcode != NSAPI_ERROR_OK) {
+        //
+        // Open socket
+        //
+        retcode = sock.open(&iface);
+        if (retcode != NSAPI_ERROR_OK) {
 #if MBED_CONF_APP_SOCK_TYPE == TCP
             snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.open() fails, code: %d\n", retcode);
             DMSG(print_text);
@@ -245,142 +245,142 @@ nsapi_error_t test_send_recv()
             snprintf(print_text, PRINT_TEXT_LENGTH, "UDPSocket.open() fails, code: %d\n", retcode);
             DMSG(print_text);
 #endif
-        	continue;
-    	}
+            continue;
+        }
 
-		snprintf(print_text, PRINT_TEXT_LENGTH, "Socket.open() success, code: %d\n", retcode);
-    	DMSG(print_text);
+        snprintf(print_text, PRINT_TEXT_LENGTH, "Socket.open() success, code: %d\n", retcode);
+        DMSG(print_text);
 
-		//
-		// Set socket address & port number
-		//
-    	retcode = iface.gethostbyname(host_name, &sock_addr);
-    	if (retcode != NSAPI_ERROR_OK) {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "Couldn't resolve remote host: %s, code: %d\n", host_name,
-               	retcode);
-        	DMSG(print_text);
-        	continue;
-    	}
+        //
+        // Set socket address & port number
+        //
+        retcode = iface.gethostbyname(host_name, &sock_addr);
+        if (retcode != NSAPI_ERROR_OK) {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "Couldn't resolve remote host: %s, code: %d\n", host_name,
+                   retcode);
+            DMSG(print_text);
+            continue;
+        }
 
-		snprintf(print_text, PRINT_TEXT_LENGTH, "remote host: %s, code: %d\n", host_name, retcode);
-   		DMSG(print_text);
+        snprintf(print_text, PRINT_TEXT_LENGTH, "remote host: %s, code: %d\n", host_name, retcode);
+           DMSG(print_text);
 
-    	sock_addr.set_port(port);
-    	sock.set_timeout(SEND_DATA_TIMEOUT);
-	
-    	//
-    	// Connect to server
-    	//
+        sock_addr.set_port(port);
+        sock.set_timeout(SEND_DATA_TIMEOUT);
+    
+        //
+        // Connect to server
+        //
 #if MBED_CONF_APP_SOCK_TYPE == TCP
-    	retcode = sock.connect(sock_addr);
-    	if (retcode < 0) {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.connect() fails, code: %d\n", retcode);
-        	DMSG(print_text);
-        	continue;
-    	} else {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "TCP: connected with %s server\n", host_name);
-        	DMSG(print_text);
-    	}
+        retcode = sock.connect(sock_addr);
+        if (retcode < 0) {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.connect() fails, code: %d\n", retcode);
+            DMSG(print_text);
+            continue;
+        } else {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "TCP: connected with %s server\n", host_name);
+            DMSG(print_text);
+        }
 #endif
 
-		//
-		// Send packets to echo server
-		//
+        //
+        // Send packets to echo server
+        //
 #if MBED_CONF_APP_SOCK_TYPE == UDP
-		retcode = sock.sendto(host_name, port, (void*) echo_string, send_data_size);
-    	if (retcode < 0) {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "UDPSocket.sendto() fails, pkt #: %d, code: %d\n", i+1, retcode);
-        	DMSG(print_text);
+        retcode = sock.sendto(host_name, port, (void*) echo_string, send_data_size);
+        if (retcode < 0) {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "UDPSocket.sendto() fails, pkt #: %d, code: %d\n", i+1, retcode);
+            DMSG(print_text);
 
-			// Failed to send data and turn off the power
-			g_tPower = 0;
+            // Failed to send data and turn off the power
+            g_tPower = 0;
 
-			// Wait PACKET_INTERVAL seconds before sending next packet
-			wait(PACKET_INTERVAL);
+            // Wait PACKET_INTERVAL seconds before sending next packet
+            wait(PACKET_INTERVAL);
 
-			// Don't exit. Continue with the next packet
-			continue;		
-    	} else {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "UDP: Sent %d Bytes to %s ater tries %d\n", retcode, host_name, i + 1);
-        	DMSG(print_text);
-    	}
+            // Don't exit. Continue with the next packet
+            continue;        
+        } else {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "UDP: Sent %d Bytes to %s ater tries %d\n", retcode, host_name, i + 1);
+            DMSG(print_text);
+        }
 
-		//
-		// Wait for data received
-		//
-		memset(recv_buf, 0, sizeof(recv_buf));
-    	retcode = sock.recvfrom(&sock_addr, (void*) recv_buf, sizeof(recv_buf));
-    	if (retcode > 0) {
+        //
+        // Wait for data received
+        //
+        memset(recv_buf, 0, sizeof(recv_buf));
+        retcode = sock.recvfrom(&sock_addr, (void*) recv_buf, sizeof(recv_buf));
+        if (retcode > 0) {
             snprintf(print_text, PRINT_TEXT_LENGTH, "UDP: Received %d Bytes \"%s\" from %s tries: %d\n", retcode, recv_buf, host_name, i + 1);
             DMSG(print_text);
-    	} else {
+        } else {
             snprintf(print_text, PRINT_TEXT_LENGTH, "UDPSocket.recvfrom() fails after %d tries, code: %d\n", i + 1, retcode);
             DMSG(print_text);
-    	}
-		
+        }
+        
 #else // TCP
-    	retcode = sock.send((void*) echo_string, send_data_size);
-    	if (retcode < 0) {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.send() fails, code: %d\n", retcode);
-        	DMSG(print_text);
-			
-			// Failed to send data and turn off the power
-			g_tPower = 0;
+        retcode = sock.send((void*) echo_string, send_data_size);
+        if (retcode < 0) {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.send() fails, code: %d\n", retcode);
+            DMSG(print_text);
+            
+            // Failed to send data and turn off the power
+            g_tPower = 0;
 
-			// Wait PACKET_INTERVAL seconds before sending next packet
-			wait(PACKET_INTERVAL);
+            // Wait PACKET_INTERVAL seconds before sending next packet
+            wait(PACKET_INTERVAL);
 
-			// Don't exit. Continue with the next packet
-			continue;
-    	} else {
-        	snprintf(print_text, PRINT_TEXT_LENGTH, "TCP: Sent %d Bytes to %s\n", retcode, host_name);
-        	DMSG(print_text);
-    	}
+            // Don't exit. Continue with the next packet
+            continue;
+        } else {
+            snprintf(print_text, PRINT_TEXT_LENGTH, "TCP: Sent %d Bytes to %s\n", retcode, host_name);
+            DMSG(print_text);
+        }
 
-		//
-		// Wait for data received
-		//
-		memset(recv_buf, 0, sizeof(recv_buf));
-    	retcode = sock.recv((void*) recv_buf, send_data_size);
-    	if (retcode > 0) {
+        //
+        // Wait for data received
+        //
+        memset(recv_buf, 0, sizeof(recv_buf));
+        retcode = sock.recv((void*) recv_buf, send_data_size);
+        if (retcode > 0) {
             snprintf(print_text, PRINT_TEXT_LENGTH, "TCP: Received %d Bytes \"%s\" from %s tries: %d\n", retcode, recv_buf, host_name, i + 1);
             DMSG(print_text);
-    	} else {
+        } else {
             snprintf(print_text, PRINT_TEXT_LENGTH, "TCPSocket.recvfrom() fails after %d tries, code: %d\n", i + 1, retcode);
             DMSG(print_text);
-    	}
+        }
 
 #endif // end if MBED_CONF_APP_SOCK_TYPE
 
         // 
         // If MCU cannot go into deep-sleep, unlock it; so it can go into deep-sleep during idle
-		//
-		while (!sleep_manager_can_deep_sleep()) {
-        	sleep_manager_unlock_deep_sleep();
-			break;
-		}
+        //
+        while (!sleep_manager_can_deep_sleep()) {
+            sleep_manager_unlock_deep_sleep();
+            break;
+        }
 
-		//
-		// Close socket and turn off the power
-		//
-		DMSG("Close socket!\n\n");
-    	sock.close();
-		g_tPower = 0;
+        //
+        // Close socket and turn off the power
+        //
+        DMSG("Close socket!\n\n");
+        sock.close();
+        g_tPower = 0;
 
-		//
-		// MCU can go into deep sleep
-		//
-		ThisThread::sleep_for(MCU_SLEEP_TIME);
-		modemUART->set_baud(MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+        //
+        // MCU can go into deep sleep
+        //
+        ThisThread::sleep_for(MCU_SLEEP_TIME);
+        modemUART->set_baud(MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
         // Delay to restore lpuart
         wait(1);
 
-		//
+        //
         // Lock deep-sleep; so to prevent MCU from go into deep-sleep mode
-		//
+        //
         sleep_manager_lock_deep_sleep();
 
-		i++;
+        i++;
 
     }
 
@@ -394,20 +394,20 @@ nsapi_error_t test_send_recv()
 //*****************************************************************************
 int main()
 {
-	nsapi_error_t retcode;
+    nsapi_error_t retcode;
 
 #if MBED_TICKLESS
-	DMSG("MBED_TICKLESS is enabled\n");
+    DMSG("MBED_TICKLESS is enabled\n");
 #else
-	DMSG("MBED_TICKLESS is disabled\n");
+    DMSG("MBED_TICKLESS is disabled\n");
 #endif
 
-	// Delay for modem ready
-	ThisThread::sleep_for(5000);
+    // Delay for modem ready
+    ThisThread::sleep_for(5000);
 
     // Turn on power for periphrals
-	g_tPower = 1;
-	
+    g_tPower = 1;
+    
     // initialized trace log
     mbed_trace_init();
     iface.modem_debug_on(MBED_CONF_APP_MODEM_TRACE);
@@ -416,18 +416,18 @@ int main()
     iface.set_sim_pin(MBED_CONF_APP_SIM_PIN_CODE);
 
     // Set network credentials here, e.g., APN
-	iface.set_credentials(MBED_CONF_APP_APN, MBED_CONF_APP_USERNAME, MBED_CONF_APP_PASSWORD);
+    iface.set_credentials(MBED_CONF_APP_APN, MBED_CONF_APP_USERNAME, MBED_CONF_APP_PASSWORD);
 
     // Set PSM
-	power = iface.get_device()->open_power(iface.get_serial());
-	power->opt_power_save_mode(PSM_PERIODIC_TIME, PSM_ACTIVE_TIME);
-	snprintf(print_text, PRINT_TEXT_LENGTH, 
-			"\n\rPower setting: PSM:TAU->%d seconds, ActiveTime->%d seconds, MCU Sleep time:%d seconds\n\r", 
-			PSM_PERIODIC_TIME, 
-			PSM_ACTIVE_TIME, 
-			MCU_SLEEP_TIME/1000);
-   	DMSG(print_text);
-	
+    power = iface.get_device()->open_power(iface.get_serial());
+    power->opt_power_save_mode(PSM_PERIODIC_TIME, PSM_ACTIVE_TIME);
+    snprintf(print_text, PRINT_TEXT_LENGTH, 
+            "\n\rPower setting: PSM:TAU->%d seconds, ActiveTime->%d seconds, MCU Sleep time:%d seconds\n\r", 
+            PSM_PERIODIC_TIME, 
+            PSM_ACTIVE_TIME, 
+            MCU_SLEEP_TIME/1000);
+            DMSG(print_text);
+    
 
     DMSG("\n\nmbed-os-example-cellular\n");
     DMSG("Establishing connection ");
